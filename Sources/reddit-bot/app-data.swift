f97a68ec25d1be59a4245f3874834862c585367a
MacAir AM <iammorjj@gmail.com>
2020-09-01 12:53:25 +0300

    Get auth response

---
+++ Sources/reddit-bot/app-data.swift
@@ -0,0 +1,15 @@
+//
+//  app-data.swift
+//  reddit-bot
+//
+//  Created by Alexander Mordovsky on 01/09/2020.
+//  Copyright © 2020 Alexander Mordovsky. All rights reserved.
+//
+
+import Foundation
+
+let username = "walrus_bot"
+let password = "123456Sq"
+
+let appID = "4GCECPoGN7C7ww"
+let appSecret = "mDAw6kwr7ae9uTRdG31Uj8oC2-s"
--- Sources/reddit-bot/main.swift
+++ Sources/reddit-bot/main.swift
@@ -1,1 +1,21 @@
-print("Hello, world!")
+import Foundation
+import Alamofire
+
+let urlString = "https://www.reddit.com/api/v1/access_token"
+
+let params = [
+  "grant_type":"password",
+  "username":"\(username)",
+  "password":"\(password)"
+]
+
+let headers: HTTPHeaders = [
+  "User-Agent": "ChangeMeClient/0.1 by YourUsername",
+]
+
+Alamofire.request(urlString, method: .post, parameters: params, encoding: URLEncoding.default, headers: headers).authenticate(user: appID, password: appSecret).responseJSON { response in
+  debugPrint(response)
+  exit(EXIT_SUCCESS)
+}
+
+dispatchMain()
