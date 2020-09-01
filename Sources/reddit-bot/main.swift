c4c871f98f6620d9395b885c7c6e66e8d3ed67d0
Alexander M <iammorjj@gmail.com>
2020-09-01 23:42:39 +0300

    Parsed subreddit post.

--- Sources/reddit-bot/main.swift
+++ Sources/reddit-bot/main.swift
@@ -1,21 +1,75 @@
 import Foundation
 import Alamofire
 
-let urlString = "https://www.reddit.com/api/v1/access_token"
+var token = ""
 
-let params = [
-  "grant_type":"password",
-  "username":"\(username)",
-  "password":"\(password)"
-]
+func getNewToken() {
+  let urlString = "https://www.reddit.com/api/v1/access_token"
 
-let headers: HTTPHeaders = [
-  "User-Agent": "ChangeMeClient/0.1 by YourUsername",
-]
+  let params = [
+    "grant_type": "password",
+    "username": "\(username)",
+    "password": "\(password)"
+  ]
 
-Alamofire.request(urlString, method: .post, parameters: params, encoding: URLEncoding.default, headers: headers).authenticate(user: appID, password: appSecret).responseJSON { response in
-  debugPrint(response)
-  exit(EXIT_SUCCESS)
+  Alamofire.request(urlString, method: .post, parameters: params).authenticate(user: appID, password: appSecret).responseJSON { response in
+    guard response.result.isSuccess else {
+      print("no success")
+      exit(EXIT_SUCCESS)
+    }
+    
+    guard let dict = response.result.value as? [String: Any] else {
+      print("no value transform")
+      exit(EXIT_SUCCESS)
+    }
+    
+    token = dict["access_token"] as! String
+    
+    getNewMeme()
+  }
 }
+
+func getNewMeme(subreddit: String = "MedicalMeme") {
+  let url = "https://oauth.reddit.com/r/\(subreddit)/hot?limit=1"
+  
+//  let params = [
+//    "grant_type": "password",
+//    "username": "\(username)",
+//    "password": "\(password)"
+//  ]
+  
+  let headers: HTTPHeaders = [
+    "Authorization": "Bearer \(token)",
+  ]
+  
+  Alamofire.request(url, headers: headers).responseJSON { response in
+    guard response.result.isSuccess else {
+      print("[getNewMeme] no success")
+      exit(EXIT_SUCCESS)
+    }
+
+    guard let dict = response.result.value as? [String: Any],
+      let globData = dict["data"] as? [String: Any],
+      let children = globData["children"] as? [[String: Any]],
+      let child = children.first,
+      let data = child["data"] as? [String: Any] else {
+        print("[getNewMeme] no value transform")
+        exit(EXIT_SUCCESS)
+    }
+
+    let url = data["url"] as? String
+    let after = data["after"] as? String
+    let postID = data["id"] as? String
+    
+    let gag = "bebebe"
+    
+    print(url ?? gag)
+    print(after ?? gag)
+    print(postID ?? gag)
+    
+  }
+}
+
+getNewToken()
 
 dispatchMain()
