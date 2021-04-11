c41c82e41f38b6742eac7ae893a515f36b7aeac8
Alexander Mordovsky <iammorjj@gmail.com>
2021-04-11 23:21:39 +0300

    Get auth response

---
+++ Sources/reddit-bot/app-data.swift
@@ -0,0 +1,15 @@
+//
+//  app-data.swift
+//  reddit-bot
+//
+//  Created by Alexander Mordovsky on 01/09/2020.
+//  Copyright © 2020 Alexander Mordovsky. All rights reserved.
+//
+
+import Foundation
+
+let username = ""
+let password = ""
+
+let appID = ""
+let appSecret = ""
